a4252b4d22bcecb2aca15374b019b461cd5bc196
strider99 <panigrahiomprakash@gmail.com>
2015-11-23 17:28:39 +0530

    add rating stars to images

---
+++ image_share.js
@@ -0,0 +1,37 @@
+Images = new Mongo.Collection("images");
+
+
+
+if (Meteor.isClient) {
+
+
+  Template.images.helpers({
+    imgarray: Images.find({}, {sort: {rating: -1}})
+  });
+
+  Template.images.events({
+    'click .js-image': function(event) {
+      $(event.target).css("width", "50px");
+
+    },
+    'click .js-del-image': function(event) {
+      var image_id = this._id;
+      console.log(image_id);
+      $("#" + image_id).hide('slow', function() {
+        Images.remove({
+          "_id": image_id
+        });
+
+      });
+    },
+    'click .js-rate-image': function(event) {
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.id;
+      console.log(image_id);
+      Images.update({_id:image_id},
+         {$set: {rating:rating}});
+    }
+  });
+
+}
