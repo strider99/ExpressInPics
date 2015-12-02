2d0321c9009180de38f835b6198274484cd61399
strider99 <panigrahiomprakash@gmail.com>
2015-12-02 12:36:02 +0530

    Modify code organization.

--- server/server.js
+++ server/server.js
@@ -1,1 +1,16 @@
-console.log("running on the server");
+Meteor.startup(function () {
+    if (Images.find().count() == 0) {
+
+        for (var i = 1; i < 23; i++) {
+            Images.insert({
+                    img_src: "img_" + i + ".jpg",
+                    img_alt: "image" + i
+                }
+
+            );
+        } //end of for insert images
+        // count the i
+
+        console.log("startup.js says " + Images.find().count());
+    } // end of if have no images
+});
